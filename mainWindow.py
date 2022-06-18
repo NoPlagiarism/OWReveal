a8f0ab4f18300e65be4400b899ed72b16846b739
NoPlagiarism <37241775+NoPlagiarism@users.noreply.github.com>
2022-06-19 04:31:22 +0500

    Draft of main window is ready

--- mainWindow.py
+++ mainWindow.py
@@ -1,8 +1,17 @@
 from PyQt6.QtCore import Qt, QRect, pyqtSignal, pyqtSlot, QTimer
-from PyQt6.QtWidgets import (QMainWindow, QWidget, QLabel, QComboBox, QPushButton, QLineEdit, QApplication,
-                             QGridLayout, QHBoxLayout, QVBoxLayout, QLayoutItem, QSizePolicy)
+from PyQt6.QtWidgets import (QMainWindow, QWidget, QLabel, QComboBox, QPushButton, QLineEdit, QApplication, QCheckBox,
+                             QGridLayout, QHBoxLayout, QVBoxLayout, QLayoutItem, QSizePolicy, QSpacerItem)
+from PyQt6.QtGui import QPixmap
 from stylesheet import DARK_STYLE
+from PIL import Image
+from PIL.ImageQt import ImageQt
 import sys
+from enum import IntEnum
+
+
+class PlayerSide(IntEnum):
+    CT = 0
+    T = 1
 
 
 class IndicLabel(QLabel):
@@ -97,6 +106,114 @@ class PrevLinkTimer(QLabel):
         self.setText(f"LINK found {self.seconds} minutes ago")
 
 
+class Player(QWidget):
+    def __init__(self, *args, side, **kwargs):
+        super(Player, self).__init__(*args, **kwargs)
+
+        self.rank_translate = dict()
+        for i in range(19):
+            im = Image.open(rf"resources\csgo_rank_icons\{0}.png")
+            im = im.resize((46, 26))
+            self.rank_translate[i] = QPixmap.fromImage(ImageQt(im))
+
+        self.lay = QHBoxLayout(self)
+        self.setLayout(self.lay)
+
+        self.checkbox = QCheckBox()
+
+        self.name = QLabel("???")
+        self.name.setStyleSheet("color: blue;") if side == PlayerSide.CT else self.name.setStyleSheet("color: red;")
+
+        self.icon = QLabel()
+        self.icon.setFixedSize(46, 26)
+        self.icon.setPixmap(self.rank_translate[0])
+
+        self.kad = QLabel("0/0/0")
+        self.setup_ui_ct() if side == PlayerSide.CT else self.setup_ui_t()
+
+    def setup_ui_ct(self):
+        self.lay.addWidget(self.checkbox, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.lay.addWidget(self.name, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.lay.addWidget(self.icon, alignment=Qt.AlignmentFlag.AlignRight)
+        self.lay.addWidget(self.kad, alignment=Qt.AlignmentFlag.AlignRight)
+
+    def setup_ui_t(self):
+        self.lay.addWidget(self.kad, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.lay.addWidget(self.icon, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.lay.addWidget(self.name, alignment=Qt.AlignmentFlag.AlignRight)
+        self.lay.addWidget(self.checkbox, alignment=Qt.AlignmentFlag.AlignRight)
+
+
+class RoundStatsWidget(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(RoundStatsWidget, self).__init__(*args, **kwargs)
+        self.rank_translate = dict()
+        for i in range(19):
+            self.rank_translate[i] = QPixmap(rf"resources\csgo_rank_icons\{i}.png")
+
+        self.main_lay = QHBoxLayout(self)
+        self.setLayout(self.main_lay)
+        self.ct_lay = QVBoxLayout()
+        self.t_lay = QVBoxLayout()
+        self.main_lay.addLayout(self.ct_lay)
+        self.main_lay.addLayout(self.t_lay)
+
+    def testy(self):
+        for _ in range(5):
+            self.ct_lay.addWidget(Player(side=0))
+        for _ in range(5):
+            self.t_lay.addWidget(Player(side=1))
+
+
+class Kill(QWidget):
+    def __init__(self, *args, p1=None, weapon=None, p2=None, **kwargs):
+        super(Kill, self).__init__(*args, **kwargs)
+
+        self.lay = QHBoxLayout(self)
+        self.setLayout(self.lay)
+        self.p1_lbl = QLabel()
+        self.weapon = QLabel()
+        self.p2_lbl = QLabel()
+
+        self.p1_lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.weapon.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.p2_lbl.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.lay.addWidget(self.p1_lbl)
+        self.lay.addWidget(self.weapon)
+        self.lay.addWidget(self.p2_lbl)
+
+        if (p1 is not None) and (weapon is not None) and (p2 is not None):
+            self.update_data(p1, weapon, p2)
+
+    def update_data(self, p1, weapon, p2):
+        """pi - (nickname, site)"""
+        self.p1_lbl.setText(p1[0])
+        self.weapon.setText(weapon)
+        self.p2_lbl.setText(p2[0])
+
+        self.p1_lbl.setStyleSheet("color: red") if p1[1] == PlayerSide.T else self.p1_lbl.setStyleSheet("color: blue")
+        self.p2_lbl.setStyleSheet("color: red") if p1[1] == PlayerSide.T else self.p1_lbl.setStyleSheet("color: blue")
+
+
+class KillsWidget(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(KillsWidget, self).__init__(*args, **kwargs)
+
+        self.main_lay = QHBoxLayout(self)
+        self.setLayout(self.main_lay)
+        self.a_lay = QVBoxLayout()
+        self.b_lay = QVBoxLayout()
+        self.main_lay.addLayout(self.a_lay)
+        self.main_lay.addLayout(self.b_lay)
+
+    def testy(self):
+        for _ in range(5):
+            self.a_lay.addWidget(Kill(p1=("PTN", PlayerSide.T), weapon="suicide", p2=("PTN", PlayerSide.CT)))
+        for _ in range(5):
+            self.b_lay.addWidget(Kill(p1=("PTN", PlayerSide.CT), weapon="suicide", p2=("PTN", PlayerSide.T)))
+
+
 class MainApp(QMainWindow):
     SIZE_X, SIZE_Y = 805, 350
     START_X = 0.005
@@ -110,7 +227,8 @@ class MainApp(QMainWindow):
     def __init__(self, title):
         super().__init__()
         self.setWindowTitle(title)
-        self.setMinimumSize(self.SIZE_X, self.SIZE_Y)
+        self.setFixedSize(self.SIZE_X, self.SIZE_Y)
+        self.stats_active = True
 
         # INIT UI
 
@@ -120,7 +238,7 @@ class MainApp(QMainWindow):
         self.lbl_indicator = IndicLabel(self)
         self.lbl_indicator.stopped = True
 
-        self.start_btn = ToggleScanBtn(self)
+        self.start_btn = ToggleScanBtn("Start", parent=self)
         self.start_btn.stopped = True
 
         # TODO: MAKE A MENU ON RIGHT CLICK (COPY URL | COPY DEMO NAME)
@@ -133,6 +251,7 @@ class MainApp(QMainWindow):
         self.prev_link_timer.reset_timer_sig.emit()
 
         self.switch_stats_kills = QPushButton("Show Kills", parent=self)
+        self.switch_stats_kills.clicked.connect(self._toggle_stats_frame)
 
         self.download_demo = QPushButton("Download DEMO", parent=self)
 
@@ -158,6 +277,17 @@ class MainApp(QMainWindow):
 
         self.show_settings = QPushButton("Settings", parent=self)
 
+        self.ct_score_lbl = QLabel("0", parent=self)
+        self.t_score_lbl = QLabel("0", parent=self)
+
+        self.round_stats = RoundStatsWidget(parent=self)
+        self.round_stats.testy()
+
+        self.kills = KillsWidget(parent=self)
+        self.kills.testy()
+        if self.stats_active:
+            self.kills.hide()
+
         self.update_geometry()
 
     def update_geometry(self):
@@ -178,9 +308,24 @@ class MainApp(QMainWindow):
         self.show_watchlist.setGeometry(self.get_geometry_from_percent(0.8, 0.78, 0.19, 0.08))
         self.show_links.setGeometry(self.get_geometry_from_percent(0.8, 0.87, 0.09, 0.08))
         self.show_settings.setGeometry(self.get_geometry_from_percent(0.9, 0.87, 0.09, 0.08))
+        self.ct_score_lbl.setGeometry(self.get_geometry_from_percent(0.15, 0.42, 0.1, 0.08))
+        self.t_score_lbl.setGeometry(self.get_geometry_from_percent(0.65, 0.42, 0.1, 0.08))
+        self.round_stats.setGeometry(self.get_geometry_from_percent(0, 0.5, 0.75, 0.49))
+        self.kills.setGeometry(self.get_geometry_from_percent(0, 0.5, 0.75, 0.49))
 
     def resizeEvent(self, a0) -> None:
         self.update_geometry()
+
+    def _toggle_stats_frame(self):
+        if self.stats_active:
+            self.round_stats.hide()
+            self.kills.show()
+            self.switch_stats_kills.setText("Show Stats")
+        else:
+            self.kills.hide()
+            self.round_stats.show()
+            self.switch_stats_kills.setText("Show Kills")
+        self.stats_active = not self.stats_active
 
 
 if __name__ == '__main__':
